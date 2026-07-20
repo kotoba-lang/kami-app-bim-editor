64418623c171f22ebc11237a8482b4f45c1f5baf
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-07-21 00:58:23 +0900

    feat: add rotate mirror and array editing

--- test/kami/bim_editor/ui_test.clj
+++ test/kami/bim_editor/ui_test.clj
@@ -7,6 +7,9 @@
   (let [page (ui/page)]
     (doseq [id ["add-wall" "add-family-instance" "analyze-structure" "route-pipe"
                 "move-element" "copy-element" "move-x" "move-y" "move-z"
+                "rotate-element" "mirror-element" "array-element" "transform-angle"
+                "pivot-x" "pivot-y" "pivot-z" "mirror-x" "mirror-y" "mirror-z"
+                "array-kind" "array-count"
                 "add-room" "quantity-schedule" "clash-results" "drawing-kind"
                 "drawing-format" "export-drawing" "import-ifc-file"]]
       (is (string/includes? page (str "id=\"" id "\""))))
