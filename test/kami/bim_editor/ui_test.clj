3fc007828acbac3b4b0f9095f7923e73e15ad845
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-07-21 00:52:11 +0900

    feat: add numeric move and copy editing (#26)

--- test/kami/bim_editor/ui_test.clj
+++ test/kami/bim_editor/ui_test.clj
@@ -6,6 +6,7 @@
 (deftest generated-ui-retains-complete-authoring-workspace
   (let [page (ui/page)]
     (doseq [id ["add-wall" "add-family-instance" "analyze-structure" "route-pipe"
+                "move-element" "copy-element" "move-x" "move-y" "move-z"
                 "add-room" "quantity-schedule" "clash-results" "drawing-kind"
                 "drawing-format" "export-drawing" "import-ifc-file"]]
       (is (string/includes? page (str "id=\"" id "\""))))
